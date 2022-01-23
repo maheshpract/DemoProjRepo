5e0336fad0766742a14249b31ab7879664303faa
Dev <Dev@gmail.com>
2022-01-23 19:21:16 +0530

    dev fst cmt

--- demo/src/demo/Cla.java
+++ demo/src/demo/Cla.java
@@ -1,7 +1,9 @@
 package demo;
 
+
 public class Cla {
 public static void main(String[] args) {
 	System.out.println(10);
+	System.out.println("max");
 }
 }
---
+++ demo/src/demo/ClassX.java
@@ -0,0 +1,7 @@
+package demo;
+
+public class ClassX {
+public static void main(String[] args) {
+	
+}
+}
